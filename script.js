b643a7cf739a0bb445cdb0ead961f73c79447ba0
Abdul-Rehman67 <ar6373150@gmail.com>
2021-05-29 06:43:58 +0500

    footer

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ $(document).ready(function() {
         $('.navbar').toggleClass('nav-toggle');
     })
 })
+$('section').each(function(){
+    let height = $(this).height();
+    let offset = $(this).offset().top - 200;
+    let top = $(window).scrollTop();
+     
+    let id = $(this).attr('id');
+    if(top > offset && top < offset +height){
+        $('.navbar ul li a ').removeClass('active');
+        $('.navbar').find(`[href="#${id}"]`).addClass('active')
+    }
+
+});
